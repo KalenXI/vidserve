884d7b8a5ab9a3a0c64787f725e8f8f27d13f8ed
Kevin Vinck <chiklit@gmail.com>
2021-09-08 00:05:57 -0400

    Add video player.

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,26 +6,26 @@ export default {
     titleTemplate: '%s - vidserve',
     title: 'vidserve',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  server: {
+    host: '0', // default: localhost
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -60,13 +60,12 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
---
+++ pages/watch/_id.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container style="max-width: 90em">
+    <video-player :options="videoOptions" class="mx-xl-15" />
+    <v-row class="my-5 mx-xl-12">
+      <v-col style="flex-grow: 5">
+        <v-card class="pa-4">
+          <h1>Title Goes Here</h1>
+          <div>Recorded On: 7/24/2019</div>
+          <div>Length: 5:50</div>
+        </v-card>
+        <div style="margin-top: 1em">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
+          aut, eos esse facere ipsam magnam numquam quisquam repellat ullam
+          voluptate! Ab accusantium alias ex mollitia nulla porro provident quos
+          vitae.
+        </div>
+      </v-col>
+      <v-col class="d-flex justify-end">
+        <div class="d-flex flex-column" style="width: 100%">
+          <v-btn width="100%" class="mb-4">Download</v-btn>
+          <v-btn width="100%">Share</v-btn>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import VideoPlayer from '@/components/VideoPlayer.vue'
+// import 'video.js/dist/video-js.css'
+
+export default {
+  name: 'TestVideo',
+  components: {
+    VideoPlayer,
+  },
+  asyncData({ params }) {
+    const id = params.id
+    return { id }
+  },
+  data() {
+    return {
+      videoOptions: {
+        autoplay: false,
+        controls: true,
+        sources: [
+          {
+            src: '/videos/' + this.$nuxt.$route.params.id + '/master.m3u8',
+            type: 'application/x-mpegURL',
+          },
+        ],
+      },
+    }
+  },
+}
+</script>
